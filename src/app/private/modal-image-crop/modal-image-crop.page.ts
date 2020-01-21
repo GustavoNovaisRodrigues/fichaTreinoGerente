6a24be3cfe660870c305afc0b85a62607d2407e1
Gustavo Novais Rodrigues <gustavonovaisrodrigues@gmail.com>
2020-01-21 05:01:35 -0300

    Finalizado o editor de Cropp image

--- src/app/private/modal-image-crop/modal-image-crop.page.ts
+++ src/app/private/modal-image-crop/modal-image-crop.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import 'cropperjs/dist/cropper.css';
 import Cropper from "cropperjs";
@@ -12,13 +12,24 @@ export class ModalImageCropPage implements OnInit {
 
   @ViewChild("image", { static: false })
   public stage: ElementRef;
+  @ViewChild("modal", { static: false })
+  public modalBox: ElementRef;
 
+  public botoesDeEdicao: Array<any> = [
+    { icon: 'refresh', title: 'Girar', checked: true },
+    { icon: 'move', title: 'Direção', checked: false },
+    { icon: 'search', title: 'Zoom In/Out', checked: false },
+    { icon: 'swap', title: 'Virar', checked: false },
+    { icon: 'crop', title: 'Cortar /Enviar', checked: false },
+  ]
+  public indexBotaoPressionado: number = 0
   public imagemCortada: string;
   public imagemParaEditar: string = "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=" //bg black 1px/1px
+
   private cropper: Cropper;
   private x: number; private y: number; private width: number; private height: number; private rotate: number; private scaleX: number; private scaleY: number;
-  private zoomNumber = 0
-  private valorRange = 0
+  private zoomNumber: number = 0
+  private valorRange: number = 0
 
   constructor(private modalController: ModalController) {
     this.imagemCortada = "";
@@ -26,21 +37,39 @@ export class ModalImageCropPage implements OnInit {
 
   ngOnInit() {
   }
-  carregarTeste() { //TESTE
-    console.log('sssssss', this.valorRange);
-    this._resetStatusRange()
-    this.imagemParaEditar = "../../../assets/img/picture.jpg"
-    this.stage.nativeElement.src = this.imagemParaEditar
-    if (this.cropper) this.cropper.destroy()
-    this._createCropper()
-  }
-
-
   //==========================================================
   //             Metodos
   //==========================================================  
   fechar() {
     this.modalController.dismiss()
+  }
+
+
+
+  enviar() {
+    console.log('[ENVIAR IMAGEM]');
+  }
+
+
+
+  cancelarModalPreview() {
+    const indexBotaoCortar = this.botoesDeEdicao.findIndex((params) => params.title === 'Cortar /Enviar')
+    if (indexBotaoCortar) this.botoesDeEdicao[indexBotaoCortar].checked = false
+    this._fadeOutModalPreview(this.modalBox.nativeElement)
+  }
+
+
+
+  selectTabButtons(index) {
+    this.botoesDeEdicao.forEach(element => element.checked = false);
+    this.botoesDeEdicao[index].checked = true
+    this.indexBotaoPressionado = index
+    if (this.botoesDeEdicao[index].title === 'Cortar /Enviar' && this.cropper) {
+      this._fadeInModalPreview(this.modalBox.nativeElement)
+      if (this.cropper) this.setPreviewDoCorte()
+    } else {
+      this._fadeOutModalPreview(this.modalBox.nativeElement)
+    }
   }
 
 
@@ -216,12 +245,31 @@ export class ModalImageCropPage implements OnInit {
 
 
 
-}
+  private _fadeOutModalPreview(el) {
+    el.style.opacity = 1;
+    (function fade() {
+      if ((el.style.opacity -= .1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
 
-// statusCroop() {
-//   console.log('[[]]', this.cropper.getCanvasData());
-//   const y = (this.cropper.getCanvasData().height / 2) + (this.cropper.getCanvasData().top)
-//   const x = (this.cropper.getCanvasData().width / 2) + (this.cropper.getCanvasData().left)
-//   console.log('[[posicao Y]]', y);
-//   console.log('[[posicao X]]', x);
-// }
+
+
+  private _fadeInModalPreview(el, display?) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+
+
+
+}
